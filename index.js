c9fde926330d0d30e0352558efe1ea9660c7581e
monovenu <673236062@qq.com>
2020-04-23 21:27:37 +0800

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,124 +1,38 @@
-//排序数组 - 给定一个整数数组 nums，将该数组升序排列。
-
-let array = [5, 1, 3, 2, 0, 6, 2];
-
-function sortArrayBubble(array) {
-  let n = array.length;
-  for (let i = n - 1; i > 0; i--) {
-    for (let j = 0; j < i; j++) {
-      if (array[j] > array[j + 1]) {
-        let temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-      }
-    }
-  }
-  return array;
+//实现bind call apply
+function bind(fn, o, ...args) {
+  return function() {
+    fn.apply(o, args);
+  };
 }
 
-// console.log(sortArrayBubble(array));
+var obj = {
+  a: 1,
+  b: 3
+};
 
-function chooseSort(array) {
-  let n = array.length;
-  for (let i = 0; i < n; i++) {
-    let min = Number.MAX_VALUE;
-    let minIndex = -1;
-    for (let j = i; j < n; j++) {
-      if (array[j] < min) {
-        min = array[j];
-        minIndex = j;
-      }
-    }
-    if (minIndex != i) {
-      [array[minIndex], array[i]] = [array[i], array[minIndex]];
-    }
+function test(c) {
+  console.log(this)
+  console.log(this.a + this.b,c);
+}
+
+// bind(test,obj)();
+
+let o={
+  tessssst:function(){
+    console.log(this);
   }
 }
+// o.tessssst();
 
-function insertSort(array) {
-  let n = array.length;
-  for (let i = 1; i < n; i++) {
-    let j = i - 1;
-    let temp = array[i]; //给temp找插入位置
-    for (; j >= 0; j--) {
-      if (array[j] <= temp) {
-        break;
-      }
-      array[j + 1] = array[j];
-    }
-    array[j + 1] = temp;
-  }
+Function.prototype.call2=function(o,...args){
+  o=o||window;
+  o.fn=this; //给o添加属性test
+  const result=o.fn(...args);
+  delete o.fn;
+  return result;
 }
 
+test.call2(null,2);
+// console.log(result);
 
-
-
-function quickSort(array) {
-  quickSortHelper(array, 0, array.length - 1);
-}
-
-function quickSortHelper(array, l, r) {
-  if (l >= r) return;
-  let midIndex = l + Math.floor(Math.random() * (r - l + 1));
-  let mid = array[midIndex];
-  swap(array,midIndex, r);
-  let i=l;
-  let j=r-1;
-  while(i<=j){
-    if(array[i]<mid){
-      i++
-    }else{
-      swap(array,i,j);
-      j--;
-    }
-  }
-  swap(array,i,r);
-  quickSortHelper(array,l,i-1);
-  quickSortHelper(array,i+1,r);
-}
-
-function swap(array, a, b) {
-  let temp = array[a];
-  array[a] = array[b];
-  array[b] = temp;
-}
-
-function mergeSort(array){
-  mergerSortHelper(array,0,array.length-1);
-}
-function mergerSortHelper(array,l,r){
-  if(l>=r) return;
-  let mid=l+Math.floor((r-l)/2);
-  mergerSortHelper(array,l,mid);
-  mergerSortHelper(array,mid+1,r);
-  merge(array,l,mid,r);
-}
-function merge(array,l,mid,r){
-  let i=l;
-  let j=mid+1;
-  let result=[];
-  while(i<=mid&&j<=r){
-    if(array[i]<array[j]){
-      result.push(array[i]);
-      i++;
-    }else {
-      result.push(array[j]);
-      j++;
-    }
-  }
-  while(i<=mid){
-    result.push(array[i]);
-    i++;
-  }
-  while(j<=r){
-    result.push(array[j]);
-    j++;
-  }
-  let t=l;
-  for(let k=0;k<result.length;k++,t++){
-    array[t]=result[k];
-  }
-}
-
-mergeSort(array);
-console.log(array);
+//函数默认参数允许在没有值或undefined
